5db058d69c0c50a21663923087b8f8d54320463f
RahulKen96 <rahulKen96@gmail.com>
2025-06-19 22:16:00 +0700

    for loop

--- main.js
+++ main.js
@@ -4,8 +4,25 @@ const app = Vue.createApp({
       game: 'Call Of Duty',
       image: './assets/images/cod_satu.jpg',
       isStock: false,
-      isVisible: false,
-      isSale: true,
+      sequels: [
+        'Call Of Duty',
+        'Call Of Duty 2',
+        'Call Of Duty 3',
+      ],
+      series: [
+        {
+          id: 1,
+          name: 'Black Ops'
+        },
+        {
+          id: 2,
+          name: 'Modern Warfare'
+        },
+        {
+          id: 3,
+          name: 'World At War'
+        },
+      ],
     }
   },
 })
