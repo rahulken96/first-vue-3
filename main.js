36014fc24c68d6375aede5b37cec972e7af2484c
RahulKen96 <rahulKen96@gmail.com>
2025-06-19 21:56:32 +0700

    Belajar bind if else | show

---
+++ main.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      game: 'Call Of Duty',
+      image: './assets/images/cod_satu.jpg',
+      isStock: true,
+      isVisible: false,
+      isSale: true,
+    }
+  },
+})
