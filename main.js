518d9bb5038797465ec36c64d06983b75a03ed82
RahulKen96 <rahulKen96@gmail.com>
2025-07-21 20:52:00 +0700

    Part class binding

--- main.js
+++ main.js
@@ -11,16 +11,19 @@ const app = Vue.createApp({
           id: 1,
           name: 'Call Of Duty',
           image: './assets/images/cod_satu.jpg',
+          color: 'blue',
         },
         {
           id: 2,
           name: 'Call Of Duty 2',
           image: './assets/images/cod_dua.jpg',
+          color: 'green',
         },
         {
           id: 3,
           name: 'Call Of Duty 3',
           image: './assets/images/cod_tiga.jpg',
+          color: 'yellow',
         },
       ],
       series: [
@@ -46,8 +49,9 @@ const app = Vue.createApp({
     decrementCart() {
       this.cart -= 1
     },
-    updateImage(image) {
-      this.image = image
+    updateImage(sequel) {
+      this.image = sequel.image
+      this.game = sequel.name
     }
   },
 })
