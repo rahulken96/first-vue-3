49bcb8273886ef18687f1c857ef3537b14905afb
RahulKen96 <rahulKen96@gmail.com>
2025-08-07 21:05:49 +0700

    Computed Properties

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ const app = Vue.createApp({
     return {
       cart: 0,
       game: 'Call Of Duty',
+      brand: 'Kenz\'s Game',
       image: './assets/images/cod_satu.jpg',
-      isStock: false,
+      isStock: true,
+      isLoaded: false,
       
       sequels: [
         {
@@ -12,18 +14,21 @@ const app = Vue.createApp({
           name: 'Call Of Duty',
           image: './assets/images/cod_satu.jpg',
           color: 'blue',
+          qty: 5,
         },
         {
           id: 2,
           name: 'Call Of Duty 2',
           image: './assets/images/cod_dua.jpg',
           color: 'green',
+          qty: 0,
         },
         {
           id: 3,
           name: 'Call Of Duty 3',
           image: './assets/images/cod_tiga.jpg',
           color: 'yellow',
+          qty: 10,
         },
       ],
       series: [
@@ -42,6 +47,9 @@ const app = Vue.createApp({
       ],
     }
   },
+  mounted() {
+    this.isLoaded = true;
+  },
   methods: {
     addToCart() {
       this.cart += 1
@@ -52,6 +60,12 @@ const app = Vue.createApp({
     updateImage(sequel) {
       this.image = sequel.image
       this.game = sequel.name
+      this.isStock = sequel.qty < 1 ? false : true
     }
   },
+  computed: {
+    title() {
+      return this.brand + ' : ' + this.game
+    }
+  }
 })
